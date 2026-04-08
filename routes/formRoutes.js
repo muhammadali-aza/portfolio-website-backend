60e254440d8e0719b1fcb87fb1d09f2273b356f5
muhammadali-aza <alirazaitservice@gmail.com>
2026-04-08 19:07:42 +0500

    add routes

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { submitForm } = require("../controllers/formController");
 
+router.post("/submit", submitForm);
+
+// CRITICAL: Make sure this line exists!
+module.exports = router;
